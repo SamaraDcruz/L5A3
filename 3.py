633a4022b172bf81317ee9cf78e4a7b8b5685b71
SamaraDcruz <samaragracedcruz@gmail.com>
2025-05-20 02:07:15 -0400

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,10 @@
+# Step 1: Ask the user to enter a number
+number = int(input("Enter a number: "))
+
+# Step 2: Check if the number is greater, smaller, or equal to 15
+if number > 15:
+    print("The number is greater than 15. 👍")
+elif number < 15:
+    print("The number is lesser than 15. 👎")
+else:
+    print("The number is equal to 15. 😃")
